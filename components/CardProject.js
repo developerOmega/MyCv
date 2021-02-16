339df1a63845f87d8e59ac3b3c46174d93a60c85
developerOmega <developerOmega@developerOmega.com>
2021-02-16 13:09:51 -0600

    Agregar estilos de card de proyectos

--- components/CardProject.js
+++ components/CardProject.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import IMG from './tags/IMG';
 import Btn from './tags/Btn';
 import BtnLink from "./tags/BtnLink";
@@ -6,14 +6,33 @@ import styles from "./CardProject.module.scss";
 
 const CardProject = ( {project} ) => {
 
+  const [active, setActive] = useState(false);
+
+  const styleContainer = active ? styles.gridOptions + " " + styles.card : styles.grid + " " + styles.card;
+  const styleButtons = active ? styles.buttonContent : styles.none;
+  const styleDesc = active ? styles.desc : styles.none;
+
+  const openOp = () =>{ 
+    if(active){
+      return;
+    }
+    setActive(true)
+    // setTimeout(() => , 500 )
+    
+  };
+
+  const closeOp = () => {
+    setActive(false)
+  };
+
   return (
-    <div className={`${styles.grid} ${styles.card}`}>
+    <div className={styleContainer} onClick={openOp}>
       <div className={styles.mainContent}> 
         <div className={styles.title}>
           {project.title}
         </div>
         
-        <div className={styles.desc + " " + styles.none} >
+        <div className={styleDesc} >
           {project.description}
         </div>
 
@@ -21,11 +40,11 @@ const CardProject = ( {project} ) => {
       <div className={styles.imgContent}>
         <IMG img={project.img} height="auto" width="128px" />
       </div>
-      <div className={ styles.none}>
+      <div className={ styleButtons }>
         <BtnLink href={`/projects/${project.id}`} >
           More ...
         </BtnLink>
-        <Btn type="button" color="danger">
+        <Btn type="button" color="danger" onClick={closeOp}>
           Exit
         </Btn>
       </div>

f7cf1396670380f0f395756f922058878cea7f3b
developerOmega <developerOmega@developerOmega.com>
2021-02-19 11:41:48 -0600

    Midificar imgs

--- components/CardSection.js
+++ components/CardSection.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import IMG from './tags/IMG';
+import Image from 'next/image';
+
 import styles from './CardSection.module.scss';
 
 const CardSection = ({section}) => {
+
+  //<IMG img={section.img} height="50" />
+  // <Image src={section.img} width="100" height="100"  layout="responsive"/>
+
   return(
     <div className={styles.card} >
-      { section.img != "" ? <IMG img={section.img} height="50" /> : <></>}
+      { section.img != "" ? <img src={section.img} /> :  <></>}
       <p> {section.content} </p>
     </div> 
   );

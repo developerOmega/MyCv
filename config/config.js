cde75d7c8a94027b56651731eb364908e650595e
developerOmega <developerOmega@developerOmega.com>
2021-02-22 10:44:59 -0600

    Cambiar variable de  de api por variable de entorno

--- config/config.js
+++ config/config.js
@@ -1,10 +1,6 @@
 const nodeEnv = process.env.NODE_ENV || 'development'; 
 
-// const url =  nodeEnv == 'development' ? 'http://localhost:4000' : 'https://mycvbackend.herokuapp.com';
-// const id = nodeEnv == 'development' ? 2 : 1;
-
-const url = 'https://mycvbackend.herokuapp.com';
-const id = 1;
-
+const url =  nodeEnv == 'development' ? 'http://localhost:4000' : process.env.URL_API;
+const id = nodeEnv == 'development' ? 2 : 1;
 
 export { url, id };

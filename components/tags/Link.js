caa1ad8fa40487628e05f4683777902ae7c39433
developerOmega <developerOmega@developerOmega.com>
2021-02-24 17:02:31 -0600

    Agregar efectos de trancicion

--- components/tags/Link.js
+++ components/tags/Link.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import styles from "./Link.module.scss";
 const Link = ( { children, href="#", target="_blank" } ) => {
-
   return (
     <a target={target} href={href} className={styles.link}> {children} </a>
-  )
+  );
 }
 
 export default Link;

859f309cc57268aa9cde1ba6ab4761ed5e2002b7
developerOmega <developerOmega@developerOmega.com>
2021-02-25 13:35:00 -0600

    Agregar iconos de social networks a compoennte

---
+++ components/icons/index.js
@@ -0,0 +1,11 @@
+import Close from './Close';
+import Left from './Left';
+import Menu from './Menu';
+import Right from './Right';
+import Fb from './Fb';
+import Gh from './Gh';
+import Gm from './Gm';
+import In from './In';
+import Sh from './Sh';
+
+export { Close, Left, Menu, Right, Fb, Gh, Gm, In, Sh};

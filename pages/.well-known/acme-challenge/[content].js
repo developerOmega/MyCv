8a2fb503d927dbd7c3bfb30ca4c0707614987d9e
developerOmega <developerOmega@developerOmega.com>
2021-02-25 17:45:27 -0600

    Agregar credenciales para sll

---
+++ pages/.well-known/acme-challenge/[content].js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const AcmeChallenge = () => {
+  return(
+    "FyCwSap5dbslgywqU_B-t1oiRejEsDheyxNJVWCYB-0.cNlfBEio5DimU5wh3IpPWdn-h9BlJSzo2yyAnpNm38U"
+  )
+}
+
+return AcmeChallenge;

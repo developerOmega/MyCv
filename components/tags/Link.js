5b860e5dd3664a4d48d009ad4455c0a5721611c4
developerOmega <developerOmega@developerOmega.com>
2021-02-25 11:06:33 -0600

    Agregra a componente link, componente link de next

--- components/tags/Link.js
+++ components/tags/Link.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import * as Li from 'next/link';
 import styles from "./Link.module.scss";
+
 const Link = ( { children, href="#", target="_blank" } ) => {
   return (
-    <a target={target} href={href} className={styles.link}> {children} </a>
+    <Li.default href={href}>
+      <a target={target} className={styles.link}> {children} </a>
+    </Li.default>
   );
 }
 

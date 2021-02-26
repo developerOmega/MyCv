e092bba9009c4cdbed8089cba58800f3a90fc4c2
developerOmega <developerOmega@developerOmega.com>
2021-02-26 12:01:32 -0600

    reescribir pagina de verificacion de sertificado ssl

--- pages/.well-known/acme-challenge/[content].js
+++ pages/.well-known/acme-challenge/[content].js
@@ -3,9 +3,8 @@ import SendLayout from '../../../Layouts/SendLayout';
 
 const AcmeChallenge = () => {
   
-  return(
+  return( 
     <>
-      PaAwSoe6Bs9GSLpTWe4oQAS-0148J4fvmB6ggXtjKe0.cNlfBEio5DimU5wh3IpPWdn-h9BlJSzo2yyAnpNm38U
     </>
   )
 }
---
+++ pages/api/acme/[content].js
@@ -0,0 +1,3 @@
+export default (req, res) => {
+  res.send("PaAwSoe6Bs9GSLpTWe4oQAS-0148J4fvmB6ggXtjKe0.cNlfBEio5DimU5wh3IpPWdn-h9BlJSzo2yyAnpNm38U");
+}

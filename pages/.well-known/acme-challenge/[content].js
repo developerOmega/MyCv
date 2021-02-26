e0b5520ae2c21f3e6311ee0bc4bb61b1602f9863
developerOmega <developerOmega@developerOmega.com>
2021-02-25 19:41:37 -0600

    Agregar users

--- pages/.well-known/acme-challenge/[content].js
+++ pages/.well-known/acme-challenge/[content].js
@@ -1,11 +1,15 @@
 import React from 'react';
+import SendLayout from '../../../Layouts/SendLayout';
 
 const AcmeChallenge = () => {
+  
   return(
     <>
       FyCwSap5dbslgywqU_B-t1oiRejEsDheyxNJVWCYB-0.cNlfBEio5DimU5wh3IpPWdn-h9BlJSzo2yyAnpNm38U
     </>
   )
 }
+
+AcmeChallenge.Layout = SendLayout
 
 export default AcmeChallenge;
